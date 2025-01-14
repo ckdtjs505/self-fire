f33153d2e609be6edc37c50799ac5c2dc4b11a84
소니아_오창선 <ckdtjs505@sooplive.com>
2025-01-14 23:10:21 +0900

    ✨ feat(header-left): toggle leftbar

--- components/header-left.tsx
+++ components/header-left.tsx
@@ -1,8 +1,12 @@
-import { Box, Text } from "@/atom";
+import { Box } from "@/atom";
 import { MaterialIcons } from "@expo/vector-icons";
+import { DrawerActions } from "@react-navigation/native";
+import { useNavigation } from "expo-router";
+import { Pressable } from "react-native";
 
 type Props = {};
 const HeaderLeft: React.FC<Props> = () => {
+  const navigation = useNavigation();
   return (
     <Box
       position={"absolute"}
@@ -14,9 +18,13 @@ const HeaderLeft: React.FC<Props> = () => {
       justifyContent={"space-between"}
       flexDirection={"row"}
     >
-      <Box m="md" p="xs">
-        <MaterialIcons name="menu" size={32}></MaterialIcons>
-      </Box>
+      <Pressable
+        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+      >
+        <Box m="md" p="xs">
+          <MaterialIcons name="menu" size={32}></MaterialIcons>
+        </Box>
+      </Pressable>
     </Box>
   );
 };
