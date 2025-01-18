11bde970e86a997e844519b0ee8e12dd0bb6f70c
소니아_오창선 <ckdtjs505@sooplive.com>
2025-01-18 11:07:54 +0900

    ✨ feat(sidebar): add sidebar content

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,7 @@
 import { Box, Text } from "@/atom";
-import { View } from "react-native";
+import { quotes } from "@/data/quotes";
+import { Alert } from "react-native";
+import { Pressable } from "react-native-gesture-handler";
 type Props = {};
 const Sidebar: React.FC<Props> = () => {
   return (
@@ -9,9 +11,44 @@ const Sidebar: React.FC<Props> = () => {
       justifyContent={"center"}
       alignItems={"center"}
     >
-      <View>
-        <Text>Sidebar</Text>
-      </View>
+      <Box>
+        <Text fontSize={24} color={"$sidebarForeground"}>
+          SELF FIRE
+        </Text>
+        <Text fontSize={18} color={"$sidebarForeground"}>
+          나를 타오르게 하는 문장들
+        </Text>
+      </Box>
+
+      <Box margin={"md"}>
+        {quotes.map(({ text }, idx) => {
+          return (
+            <Pressable
+              key={idx}
+              onPress={() => {
+                Alert.alert("삭제하시겠습니까?", "다시 되돌릴 수 없습니다,", [
+                  {
+                    text: "삭제",
+                    onPress: () => {
+                      console.log("delete");
+                    },
+                  },
+                  { text: "취소" },
+                ]);
+              }}
+            >
+              <Text
+                fontSize={16}
+                marginTop={"s"}
+                key={idx}
+                color={"$sidebarForeground"}
+              >
+                {text}
+              </Text>
+            </Pressable>
+          );
+        })}
+      </Box>
     </Box>
   );
 };
