a7e0e3352bfa197417f8ce37a7b2440f97a5a589
ckdtjs505 <ckdtjs505@naver.com>
2026-04-11 15:00:29 +0900

    fix: 명언 줄바꿈 처리 개선

--- components/quote-items.tsx
+++ components/quote-items.tsx
@@ -1,38 +1,23 @@
 import { Box, Text } from "@/atom";
 import Quotebar from "./quote-bar";
-const getChunckQuote = (text: string) => {
-  const chunckQuote: string[] = [];
-  const splitQuote = text.split(" ");
-  for (let i = 0; i < splitQuote.length; i += 3) {
-    const temp = splitQuote.slice(i, i + 3).join(" ");
-    chunckQuote.push(temp);
-  }
-
-  return chunckQuote;
-};
 type Props = {
   text: string;
   author: string;
   id: string;
 };
-const QuoteItem: React.FC<Props> = ({ text, author, id }) => {
-  const quotesTexts = getChunckQuote(text);
 
+const QuoteItem: React.FC<Props> = ({ text, author, id }) => {
   return (
     <Box flex={1} justifyContent={"center"} alignItems={"center"} margin={"lg"}>
       <Box margin={"md"}>
-        {quotesTexts.map((val, idx) => {
-          return (
-            <Text
-              key={idx}
-              fontSize={28}
-              fontWeight={"heavy"}
-              textAlign={"center"}
-            >
-              {val}
-            </Text>
-          );
-        })}
+        <Text
+          fontSize={28}
+          fontWeight={"heavy"}
+          textAlign={"center"}
+          lineHeight={40}
+        >
+          {text}
+        </Text>
       </Box>
       <Text> - {author} - </Text>
       <Quotebar id={id} text={text} author={author}></Quotebar>
